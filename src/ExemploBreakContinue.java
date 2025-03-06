9afbf56273cbba84644fd718379658bbebad3155
Luan Menezes <menezluan120@gmail.com>
2025-03-06 08:13:10 -0300

    Break and Continue

---
+++ src/ExemploBreakContinue.java
@@ -0,0 +1,12 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for (int Numero = 1;Numero <= 5; Numero++) {
+            if (Numero ==3) 
+            break;
+            System.out.println(Numero);
+            
+        }
+    }
+}
+//Break = Pause
+//Continue = Pula o numero Ex if (Numero ==3) ira imprimir 1 2 4 5
---
+++ src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        String Alunos[] = { "Luan", "Isac", "Carla", "Ian" };
+  
+        for (int x = 0; x < Alunos.length; x++) {
+
+            System.out.println("O aluno no indice X=" + x + " é " + Alunos[x]  );
+        }
+
+    }
+}
