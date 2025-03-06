3c3a3ddb953fa6dd1c5beef4c9065aeba612a3b9
Luan Menezes <menezluan120@gmail.com>
2025-03-06 07:50:23 -0300

    For Estudo

---
+++ src/Carneirinho.java
@@ -0,0 +1,11 @@
+//Exemplo For
+
+public class Carneirinho {
+    public static void main(String[] args) {
+        for (int Carneiros = 1; Carneiros <=20; Carneiros++) {
+            System.out.println("Contando Carneirinhos "+ Carneiros);
+            
+        }
+        System.out.println("Caiu no sono");
+    }
+}
