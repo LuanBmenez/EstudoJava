968de7e598c4ed80a19d4bfa89715858a1e92be6
Luan Menezes <menezluan120@gmail.com>
2025-03-05 21:56:15 -0300

    Initial commit

---
+++ src/Notinha.java
@@ -0,0 +1,17 @@
+public class Notinha{
+
+    public static void main (String[] args) {
+        int Nota = 6;
+    
+        if(Nota >= 7)
+        System.out.println("Aprovado");
+    
+        
+        else if (Nota >=5 && Nota <7)
+        System.out.println("Recuperação");  
+
+        else
+        System.out.println("Reprovado");
+        
+    }
+    }   
