d3bfe760b64b2a2e815e1adcd1b8fcf032848457
Luan Menezes <menezluan120@gmail.com>
2025-03-10 08:58:15 -0300

    DoWhile

---
+++ src/EstudoDoWhile.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class EstudoDoWhile {
+    public static void main(String[] args) {
+        System.err.println("Discando...");
+
+        do{
+            System.err.println("Telefone tocando");
+            
+        }
+        while(tocando());
+        System.err.println("Alôo !!!");
+    }
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.err.println("Atendeu? " + atendeu);
+        //para o ato de continaur tocando 
+        return ! atendeu;
+    }
+}
